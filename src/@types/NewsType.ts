69bb68f710f1f1578f97046980c5cc6cba183f4f
Vinicius Bueno Costa <buenov535@gmail.com>
2024-03-19 22:50:16 -0300

    feat: adicionado tipo para o retorno da API

---
+++ src/@types/NewsType.ts
@@ -0,0 +1,23 @@
+export type NewsType = {
+  id: number;
+  tipo: string;
+  titulo: string;
+  introducao: string;
+  data_publicacao: string;
+  produto_id: number;
+  produtos: string;
+  editorias: string;
+  imagens: {
+    image_intro: string;
+    image_intro_alt: string;
+    image_intro_caption: string;
+    image_fulltext: string;
+    image_fulltext_alt: string;
+    image_fulltext_caption: string;
+    float_intro: string;
+    float_fulltext: string;
+  };
+  produtos_relacionados: string;
+  destaque: boolean;
+  link: string;
+};
