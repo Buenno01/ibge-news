b7db3547d30817ad3d017cfa98d801aa1f2a312b
Vinicius Bueno Costa <buenov535@gmail.com>
2024-03-20 11:44:55 -0300

    feat: adicionada função que calcula quanto tempo passado da publicação da noticia

--- src/components/News/NewsDate.tsx
+++ src/components/News/NewsDate.tsx
@@ -1,10 +1,13 @@
+import getDate from '../../utils/getDate';
+
 type NewsDateProps = {
   date: string;
 };
 
 function NewsDate({ date }: NewsDateProps) {
+  const dateDifference = getDate(date);
   return (
-    <p className="text-gray-500 text-xs">{ date }</p>
+    <p className="text-gray-500 text-xs">{ dateDifference }</p>
   );
 }
 
