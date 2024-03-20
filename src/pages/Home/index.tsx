d1f3c84234f4f43752a7cdf7ea1139621a702b5c
Vinicius Bueno Costa <buenov535@gmail.com>
2024-03-20 18:47:27 -0300

    feat: adicionada lista de cards de notícias

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,27 @@
 import HighlightedNews from '../../components/HighlightedNews';
+import NewCard from '../../components/NewCard';
+import ListWrapper from '../../components/atoms/ListWrapper';
+import newsList from '../../tests/mocks/NewsList';
 
 function Home() {
   return (
-    <HighlightedNews />
+    <main className="flex flex-col gap-16">
+      <HighlightedNews />
+      <ListWrapper
+        className="xl:grid xl:grid-cols-3 md:container md:mx-auto sm:gap-y-10"
+      >
+        {
+        newsList.items.map((news, index) => (
+          <NewCard
+            key={ index }
+            news={ news }
+            index={ index }
+            loading={ false }
+          />
+        ))
+          }
+      </ListWrapper>
+    </main>
   );
 }
 
