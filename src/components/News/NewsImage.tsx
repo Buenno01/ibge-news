784fee5522d950710fddcf39df37ad237e73f268
Vinicius Bueno Costa <buenov535@gmail.com>
2024-03-20 11:56:31 -0300

    feat: agora o componente de imagem monta o próprio src com o endpoint correto

--- src/components/News/NewsImage.tsx
+++ src/components/News/NewsImage.tsx
@@ -5,7 +5,7 @@ type NewsImageProps = {
 
 function NewsImage({ src, alt }: NewsImageProps) {
   return (
-    <div className="w-full h-32 bg-red-200">
+    <div className="w-full h-32 bg-red-200 overflow-hidden">
       <img src={ src } alt={ alt } />
     </div>
   );
