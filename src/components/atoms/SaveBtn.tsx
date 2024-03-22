38002b6d2ffff32ac71841eee9900a6d00cd8b46
Vinicius Bueno Costa <buenov535@gmail.com>
2024-03-22 11:28:15 -0300

    feat: implementada funcionalidade do botao de salvar

--- src/components/atoms/SaveBtn.tsx
+++ src/components/atoms/SaveBtn.tsx
@@ -4,24 +4,23 @@ import {
   FaRegBookmark,
 } from 'react-icons/fa6';
 import { NewsType } from '../../@types/NewsType';
+import { useSavedNewsContext } from '../../context/SavedNewsContext';
 
 interface SaveBtnProps extends React.HTMLAttributes<HTMLButtonElement> {
   news: NewsType;
 }
 
 function SaveBtn({ news, ...rest }: SaveBtnProps) {
-  const [isSaved, setIsSaved] = useState(false);
-
-  const handleClick = () => {
-    setIsSaved(!isSaved);
-    console.log(`Todo: save btn${news}`);
-  };
+  const { produto_id: idProduto, id } = news;
+  const { savedNews, toggleSavedNew } = useSavedNewsContext();
+  const isSaved = savedNews
+    .some((savedNew) => savedNew[0] === idProduto && savedNew[1] === id);
 
   return (
     <button
       { ...rest }
       className="text-green-600 text-xl"
-      onClick={ handleClick }
+      onClick={ () => toggleSavedNew(idProduto, id) }
     >
       { isSaved ? <FaBookmark /> : <FaRegBookmark />}
     </button>
